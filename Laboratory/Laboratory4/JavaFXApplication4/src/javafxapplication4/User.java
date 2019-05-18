90c9884ccde0af7d8a70597d539cbc29cff4ae6b
Ilya Semakin <il.semakin@yandex.ru>
2019-05-18 20:23:42 +0300

    creating one user

---
+++ Laboratory/Laboratory4/JavaFXApplication4/src/javafxapplication4/User.java
@@ -0,0 +1,50 @@
+package javafxapplication4;
+
+public class User {
+
+    private String name;
+    private String surname;
+    private String email;
+    private final String login;
+    private final String password;
+
+    public User(String name, String surname, String email, String login, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
