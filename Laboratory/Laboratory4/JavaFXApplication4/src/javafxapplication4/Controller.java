84a5dcc79a0c39f77175cd134c512e0e6f6ac003
Ilya Semakin <il.semakin@yandex.ru>
2019-05-18 20:23:18 +0300

    controller

---
+++ Laboratory/Laboratory4/JavaFXApplication4/src/javafxapplication4/Controller.java
@@ -0,0 +1,10 @@
+package javafxapplication4;
+
+import javafx.stage.Stage;
+
+public class Controller {
+
+    public static void init(Stage primaryStage) {
+        View.showMainStage(primaryStage);
+    }
+}
