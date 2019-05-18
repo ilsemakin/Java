a0f05f6de15636b12f230bc6f60dd8d930afcbb7
Ilya Semakin <il.semakin@yandex.ru>
2019-05-19 02:48:39 +0300

    check user

--- Laboratory/Laboratory4/JavaFXApplication4/src/javafxapplication4/View.java
+++ Laboratory/Laboratory4/JavaFXApplication4/src/javafxapplication4/View.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -13,6 +14,7 @@ import javafx.stage.Stage;
 public class View {
 
     public static void showMainStage(Stage primaryStage) {
+
         VBox vbox = new VBox();
         vbox.setSpacing(10);
         vbox.setAlignment(Pos.CENTER);
@@ -27,25 +29,37 @@ public class View {
         HBox hbox_password = new HBox();
         hbox_password.setAlignment(Pos.CENTER);
         hbox_password.setSpacing(5);
-        TextField f_password = new TextField();
+        PasswordField f_password = new PasswordField();
         Label password = new Label("Пароль : ");
         hbox_password.getChildren().addAll(password, f_password);
 
-        HBox hbox_btn = new HBox();
-        hbox_btn.setAlignment(Pos.CENTER);
-        Button btn = new Button();
-        // btn.setPrefSize(200, 10);
-        btn.setText("Войти");
-        hbox_btn.getChildren().add(btn);
+        HBox account_registration = new HBox();
+        account_registration.setAlignment(Pos.CENTER);
+        account_registration.setSpacing(28);
+        Button b_account = new Button();
+        b_account.setPrefSize(90, 10);
+        b_account.setText("Войти");
+        Button b_registration = new Button();
+        b_registration.setPrefSize(90, 10);
+        b_registration.setText("Регистрация");
+        account_registration.getChildren().addAll(b_account, b_registration);
 
-        vbox.getChildren().addAll(hbox_login, hbox_password, hbox_btn);
+        vbox.getChildren().addAll(hbox_login, hbox_password, account_registration);
 
-        btn.setOnAction((ActionEvent e) -> {
+        b_account.setOnAction((ActionEvent e) -> {
+            Controller.checkUser(f_login.getText(), f_password.getText());
+        });
+
+        b_registration.setOnAction((ActionEvent e) -> {
         });
 
         primaryStage.setTitle("Вход в аккаунт");
         Scene scene = new Scene(vbox, 500, 500);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+    
+    public static void message(){
+        System.out.println("Логин или пароль введён неверно");
     }
 }
