875067a9d58f5567a2465011d5232576cbc35292
Ilya Semakin <il.semakin@yandex.ru>
2019-05-20 00:37:44 +0300

    event handling

--- Laboratory/Laboratory4/JavaFXApplication4/src/Controller/Controller.java
+++ Laboratory/Laboratory4/JavaFXApplication4/src/Controller/Controller.java
@@ -1,20 +1,29 @@
 package Controller;
 
+import Exception.FileReadException;
 import java.io.FileNotFoundException;
 import javafx.stage.Stage;
 import java.util.Scanner;
 import Model.DataBase;
 import java.io.File;
 import Model.User;
+import View.Alerts;
 import View.View;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import javafx.application.Platform;
 
 public class Controller {
 
     public static void read() {
         File input = new File("Accounts.txt");
+        ArrayList<User> list = new ArrayList<>();
+        String errorUsers = "";
         User user;
-        try {
-            Scanner in = new Scanner(input);
+
+        try (Scanner in = new Scanner(input)) {
             String str;
 
             while (in.hasNextLine()) {
@@ -22,26 +31,108 @@ public class Controller {
 
                 String line[] = str.split(" ");
                 if (line.length != 5) {
-                    in.close();
+                    errorUsers = str;
+                    throw new FileReadException();
                 }
 
                 user = new User(line[0], line[1], line[2], line[3], line[4]);
-                DataBase.addUser(user);
+                if (DataBase.checkLogin(user.getLogin()) && checkPassword(user.getPassword())) {
+                    list.add(user);
+                }
             }
             in.close();
-            DataBase.show();
+
+            list.forEach((newUser) -> {
+                DataBase.addUser(newUser);
+            });
+
         } catch (FileNotFoundException ex) {
-            System.out.println(ex.getMessage());
+            Alerts.fileRead("Файл отсутствует!\n База данных будет пустой!", errorUsers);
+        } catch (FileReadException ex) {
+            Alerts.fileRead(ex.getMessage(), errorUsers);
+            Platform.exit();
         }
     }
 
-    public static void checkUser(String login, String password) {
-        if (DataBase.find(login, password) == null) {
-            View.message();
+    public static void write() {
+        ArrayList<User> list = DataBase.getList();
+
+        if (!list.isEmpty()) {
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter("Accounts.txt"))) {
+
+                for (User user : list) {
+                    bw.write(user.getName() + " " + user.getSurname() + " " + user.getEmail()
+                            + " " + user.getLogin() + " " + user.getPassword() + "\n");
+                }
+
+                bw.close();
+            } catch (IOException ex) {
+                Alerts.fileWrite(ex.getMessage());
+            }
         }
     }
 
     public static void init(Stage primaryStage) {
         View.showMainStage(primaryStage);
+    }
+
+    public static void checkUser(String login, String password) {
+        if (DataBase.search(login, password) == null) {
+            Alerts.User();
+        }
+    }
+
+    public static void addUser(String name, String surname, String email, String login, String password) {
+        DataBase.addUser(new User(name, surname, email, login, password));
+    }
+
+    public static void registration(Stage primaryStage) {
+        View.registration(primaryStage);
+    }
+
+    public static boolean checkEmail(String email) {
+        if (email.matches("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")) {
+
+            return true;
+        } else {
+            Alerts.Email();
+            return false;
+        }
+    }
+
+    public static boolean checkLogin(String login, Stage stage) {
+        if (DataBase.checkLogin(login)) {
+            return true;
+        } else {
+            Alerts.Login(stage);
+            return false;
+        }
+    }
+
+    public static boolean checkPassword(String password, Stage stage) {
+        if (password.matches("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9@#$%]).{8,}")) {
+            return true;
+        } else {
+            Alerts.Password(stage);
+            return false;
+        }
+    }
+
+    public static boolean checkPasswords(String passwordOne, String passwordTwo) {
+        if (passwordOne.equals(passwordTwo)) {
+            return true;
+        } else {
+            Alerts.Passwords();
+            return false;
+        }
+    }
+
+    private static boolean checkPassword(String password) {
+        if (password.matches("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9@#$%]).{8,}")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
