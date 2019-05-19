244d9cbc9959582360f6c215136ecddc6023f397
Ilya Semakin <il.semakin@yandex.ru>
2019-05-20 00:38:15 +0300

    skin

--- Laboratory/Laboratory4/JavaFXApplication4/src/Model/JavaFXApplication4.java
+++ Laboratory/Laboratory4/JavaFXApplication4/src/Model/JavaFXApplication4.java
@@ -6,13 +6,18 @@ import javafx.stage.Stage;
 
 public class JavaFXApplication4 extends Application {
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start(Stage primaryStage) {
         Controller.read();
         Controller.init(primaryStage);
     }
 
-    public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void stop() {
+        Controller.write();
     }
 }
