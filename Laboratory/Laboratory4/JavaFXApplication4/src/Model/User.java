77002f16de086eb9304907f491165a38fab39ad6
Ilya Semakin <il.semakin@yandex.ru>
2019-05-20 00:38:41 +0300

    corrected code

--- Laboratory/Laboratory4/JavaFXApplication4/src/Model/User.java
+++ Laboratory/Laboratory4/JavaFXApplication4/src/Model/User.java
@@ -47,11 +47,4 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
-
-    @Override
-    public String toString() {
-        String str = "Имя: " + name + "\nФамилия: " + surname + "\nПочта: " + email;
-
-        return str;
-    }
 }
